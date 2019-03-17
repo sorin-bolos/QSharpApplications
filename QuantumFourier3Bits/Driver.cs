924aec2181585f395f7214e9d914eef54e002c30
Sorin Bolos <sorin.bolos@uipath.com>
2019-03-17 15:16:24 +0200

    Quantum Fourier Transform on 3 bits

---
+++ QuantumFourier3Bits/Driver.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.QuantumFourier3Bits
+{
+    class Driver
+    {
+        private const int NumberOfTests = 10;
+        private const int NumberOfMeasurementsPerTest = 100;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to transform: ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int number) || number < 0 || number > 7)
+            {
+                Console.WriteLine("Number must be between 0 and 7");
+                Console.ReadKey();
+                return;
+            }
+
+            var binary = ConvertToBase2(number);
+            Console.WriteLine($"Applying Fourier transform to {binary[0]}{binary[1]}{binary[2]}");
+            for (int i = 0; i < 4; i++)
+            {
+                var measurementBasis = (Pauli)i;
+                Console.WriteLine($"Measuring {measurementBasis}");
+                var results = PerformTests(binary, measurementBasis);
+                foreach (var key in results.Keys)
+                {
+                    var finalAsBinary = ConvertToBase2(key);
+                    Console.WriteLine($"Fourier transform returned {key} ({finalAsBinary[0]}{finalAsBinary[1]}{finalAsBinary[2]}) - {results[key]}%");
+                }
+            }
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+        }
+
+        static Dictionary<int, double> PerformTests(int[] binary, Pauli pauli)
+        {
+            var results = new Dictionary<int, double>();
+            for (int i = 0; i < NumberOfTests; i++)
+            {
+                var testResults = PerformTest(binary, pauli);
+                foreach (var result in testResults.Keys)
+                {
+                    if (!results.ContainsKey(result))
+                        results[result] = 0;
+                    results[result] += testResults[result] / (double)NumberOfTests;
+                }
+            }
+            return results;
+        }
+
+        static Dictionary<int, double> PerformTest(int[] binary, Pauli pauli)
+        {
+            var results = new Dictionary<int, double>();
+            for (int i = 0; i < NumberOfMeasurementsPerTest; i++)
+            {
+                var result = PerformTransform(binary, pauli);
+
+                if (!results.ContainsKey(result))
+                    results[result] = 0;
+                results[result] += 100.00 / (double)NumberOfMeasurementsPerTest;
+            }
+            return results;
+        }
+
+        static int PerformTransform(int[] binary, Pauli pauli)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                var inputAsResults = new QArray<Result>(binary.Cast<Result>());
+                var resultAsBinary = Fourier.Run(qsim, inputAsResults, pauli).Result.Cast<int>().ToArray();
+                var result = ConvertToBase10(resultAsBinary);
+                return result;
+            }
+        }
+
+        static int[] ConvertToBase2(int number)
+        {
+            var result = new int[3];
+            int index = 2;
+            while(number > 0)
+            {
+                result[index] = number % 2;
+                number = number / 2;
+                index--;
+            }
+
+            return result;
+        }
+
+        static int ConvertToBase10(int[] bits)
+        {
+            var result = 0;
+            for(int i = 0; i < bits.Length; i++)
+            {
+                result +=  (int)Math.Pow(2,(bits.Length - 1 - i)) * bits[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ QuantumFourier3Bits/Operations.qs
@@ -0,0 +1,79 @@
+﻿namespace Quantum.QuantumFourier3Bits
+{
+    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Canon;
+    
+	operation Fourier(bits: Result[], measurementBasis: Pauli) : Result[] {
+		let len = Length(bits);
+		mutable result = new Result[0];
+
+		if (len <= 0 || len > 3)
+		{
+			Message("You must supply a number encoded in 1, 2, or 3 bits");
+			return result;
+		}
+
+		using(qubits = Qubit[len]) {
+			
+			for(i in 0..len-1) {
+				Set(bits[i], qubits[i]);
+			}
+
+			if (len == 1) {
+				Fourier1Bit(qubits[0]);
+				set result = [Measure([measurementBasis], [qubits[0]])];
+			}
+			else {
+				if (len == 2) {
+					Fourier2Bit(qubits[0], qubits[1]);
+					set result = [Measure([measurementBasis], [qubits[0]]),
+					              Measure([measurementBasis], [qubits[1]])];
+				}
+				else {
+					Fourier3Bit(qubits[0], qubits[1], qubits[2]);
+					set result = [Measure([measurementBasis], [qubits[0]]),
+					              Measure([measurementBasis], [qubits[1]]),
+								  Measure([measurementBasis], [qubits[2]])];
+				}
+			}
+
+			ResetAll(qubits);
+		}
+		return result;
+	}
+
+    operation Fourier1Bit (q0: Qubit) : Unit {
+        H(q0);
+    }
+
+	operation Fourier2Bit (q0: Qubit, q1: Qubit) : Unit {
+        H(q0);
+		Controlled S([q1], q0);
+
+		H(q1);
+
+		SWAP(q0, q1);
+    }
+
+	operation Fourier3Bit (q0: Qubit, q1: Qubit, q2: Qubit) : Unit {
+        H(q0);
+		Controlled S([q1], q0);
+		Controlled T([q2], q0);
+
+		H(q1);
+		Controlled S([q2], q1);
+
+		H(q2);
+
+		SWAP(q0, q2);
+    }
+
+	operation Set(desired: Result, q1: Qubit) : Unit
+	{    
+		let current = M(q1);
+		if (desired != current)
+		{
+			X(q1);
+		}
+    }
+}
